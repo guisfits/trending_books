01506e1d2ad4328ace910b1035fb694d68dafd23
Guilherme Camargo <guisfits@hotmail.com>
2024-01-31 23:23:11 -0300

    listing trends on brazil

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from pytrends.request import TrendReq
-import pandas as pd                        
-from urllib.parse import urlparse, parse_qs
+import itertools
 
-pytrend = TrendReq()
-df = pytrend.today_searches(pn='BR')
+tzBR='-180'
+pytrend = TrendReq(hl='en-US', tz=tzBR, timeout=(10,25))
 
-trends_list = df.values.tolist()
-trends_terms = [parse_qs(urlparse(url).query)['q'][0] for url in trends_list]
+df = pytrend.trending_searches(pn='brazil')
+kw_list = list(itertools.chain.from_iterable(df.values.tolist()))
+print(kw_list)
 
+# book_category='22'
+# geo='BR'
+# last_week='now 7-d'
 
-print(trends_terms)
+# pytrend.build_payload(kw_list, cat=book_category, timeframe=last_week, geo=geo)
+# trends = pytrend.
+
+# print(trends)
