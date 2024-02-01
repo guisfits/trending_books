c077055f69aeacb57c71e72e329ddbccecd847d7
Guilherme Camargo <guisfits@hotmail.com>
2024-01-31 23:31:27 -0300

    mend

--- main.py
+++ main.py
@@ -5,14 +5,5 @@ tzBR='-180'
 pytrend = TrendReq(hl='en-US', tz=tzBR, timeout=(10,25))
 
 df = pytrend.trending_searches(pn='brazil')
-kw_list = list(itertools.chain.from_iterable(df.values.tolist()))
-print(kw_list)
-
-# book_category='22'
-# geo='BR'
-# last_week='now 7-d'
-
-# pytrend.build_payload(kw_list, cat=book_category, timeframe=last_week, geo=geo)
-# trends = pytrend.
-
-# print(trends)
+trends = list(itertools.chain.from_iterable(df.values.tolist()))
+print(trends)
