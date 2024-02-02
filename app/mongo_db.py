4066f49c27599620526a90b0d2d0bfd1add32937
Guilherme Camargo <guisfits@hotmail.com>
2024-02-01 21:17:59 -0300

    setup mongodb

---
+++ app/mongo_db.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+import os
+
+def get_database():
+    mongodb_url = os.environ.get('MONGODB_URL')
+
+    client = MongoClient(mongodb_url)
+    db = client.get_database('trends_books')
+
+    collection_names = db.list_collection_names()
+
+    if not ('books' in collection_names):
+        db.create_collection('books')
+    
+    if not ('trends' in collection_names):
+        db.create_collection('trends')
+
+    return db 
