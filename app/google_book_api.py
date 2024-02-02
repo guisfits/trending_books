03c0d57a61d8cc8ef408371c74188f9273a4236d
Guilherme Camargo <guisfits@hotmail.com>
2024-02-01 21:57:18 -0300

    saving books and trends to database

--- app/google_book_api.py
+++ app/google_book_api.py
@@ -2,7 +2,7 @@ import requests
 
 class GoogleBookAPI:
     def __init__(self):
-        self.base_url = "https://www.googleapis.com/books/v1/volumes?langRestrict=pt&orderBy=relevance&printType=BOOKS"
+        self.base_url = "https://www.googleapis.com/books/v1/volumes?orderBy=relevance&printType=BOOKS"
 
     def search_books(self, search_terms):
         url = f"{self.base_url}&q={search_terms}"
--- main.py
+++ main.py
@@ -1,13 +1,52 @@
 from app.trends import get_trends
 from app.google_book_api import GoogleBookAPI
+from dotenv import load_dotenv
+from app.mongo_db import get_database
+from datetime import datetime
 
-trends = get_trends()
+print("🚀 Starting the process..")
+
+load_dotenv()
+
+today = datetime.now().strftime('%Y%m%d')
+daily_trend = {
+    '_id': today,
+    'trends': []
+}
+
 book_api = GoogleBookAPI()
 
-for trend in trends:
-    print(f'📚 Books related to {trend}')
-    result = book_api.search_books(trend)
-    if result["totalItems"] > 0:
-        for item in result['items']:
-            print(item['volumeInfo']['title'])
+db = get_database()
+books_collection = db.get_collection('books')
+trends_collection = db.get_collection('trends')
 
+for trend in get_trends():
+    print("🔍 Searching books for trend: ", trend)
+    result = book_api.search_books(trend)
+    books_id = []
+
+    if result["totalItems"] > 0:
+        for book in result['items']:
+            print("📚 Book found: ", book['volumeInfo']['title'])
+
+            book_id = book['id']
+            books_id.append(book_id)
+            book['_id'] = book_id
+            del book['id']
+
+            existing_book = books_collection.find_one({'_id': book_id})
+            if existing_book is None:
+                books_collection.insert_one(book)
+
+            books_collection.update_one({'_id': book_id}, {'$addToSet': {'trends': trend}}, upsert=True)
+
+    if len(books_id) > 0:
+        daily_trend['trends'].append({
+            'name': trend,
+            'books': books_id
+        })
+
+print("💾 Saving trends")
+trends_collection.insert_one(daily_trend)
+
+print("✅ Done")
